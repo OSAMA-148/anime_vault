7af313a518eeabd1b3cb352265d543538a960ad7
osama <om2951855@gmail.com>
2025-05-15 17:20:55 +0300

    Refactor fetchAnime function to accept page parameter and update Home component to utilize it; add react-intersection-observer dependency

--- app/action.tsx
+++ app/action.tsx
@@ -1,13 +1,14 @@
 "use server";
 
-
-export const fetchAnime = async () => {
-    const res = await fetch("https://shikimore.one/api/animes");
+export const fetchAnime = async (page: number) => {
+    const res = await fetch(
+        `https://shikimore.one/api/animes?page=${page}&limit=8&order=popularity`
+    );
     if (!res.ok) {
         throw new Error("Failed to fetch data");
     }
-    
+
     const data = await res.json();
     console.log(data);
     return data;
-}
+};
